575751d6d332953bd283fac893339fb23c6d6c6c
ThaDocIzN <rodan72@msn.com>
2019-07-28 17:32:22 -0500

    created an interceptor to log requests

--- brav/app/src/main/java/com/thadocizn/brav/services/RetroInstance.kt
+++ brav/app/src/main/java/com/thadocizn/brav/services/RetroInstance.kt
@@ -1,6 +1,7 @@
 package com.thadocizn.brav.services
 
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -17,12 +18,15 @@ class RetroInstance {
 
     val service: BravApi
         get() {
+            val interceptor = HttpLoggingInterceptor()
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
 
             var okHttpClient: OkHttpClient = OkHttpClient().newBuilder()
 
                 .connectTimeout(120, TimeUnit.SECONDS)
                 .readTimeout(120, TimeUnit.SECONDS)
                 .writeTimeout(120, TimeUnit.SECONDS)
+                .addInterceptor(interceptor)
                 .build()
 
             var retroInstance = Retrofit.Builder()
