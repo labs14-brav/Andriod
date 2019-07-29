3d797ae498ace0d32a07650ac5adc939d050c7ef
ThaDocIzN <rodan72@msn.com>
2019-07-29 15:34:43 -0500

    created case view model

---
+++ brav/app/src/main/java/com/thadocizn/brav/viewModels/CaseViewModel.kt
@@ -0,0 +1,25 @@
+package com.thadocizn.brav.viewModels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.thadocizn.brav.models.Case
+import com.thadocizn.brav.repo.CaseRepository
+
+/**
+ * Created by charles on 24,July,2019
+ */
+class CaseViewModel(val token: String?) : ViewModel() {
+
+    private val repo: CaseRepository
+
+    init {
+        repo = CaseRepository(token)
+    }
+
+    val caseList: LiveData<List<Case>>
+        get() = repo.caseList
+
+    val createCase: LiveData<Case>
+        get() = repo.createCase
+
+}
