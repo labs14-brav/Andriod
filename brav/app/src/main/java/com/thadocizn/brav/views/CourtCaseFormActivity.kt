5e284d26b55d3ac8dccf8780ec9d4ae9cc79ba10
ThaDocIzN <rodan72@msn.com>
2019-08-06 19:36:26 -0500

    replace edit textview for case type with spinners

--- brav/app/src/main/java/com/thadocizn/brav/views/CourtCaseFormActivity.kt
+++ brav/app/src/main/java/com/thadocizn/brav/views/CourtCaseFormActivity.kt
@@ -2,11 +2,19 @@ package com.thadocizn.brav.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.thadocizn.brav.R
 import com.thadocizn.brav.models.Case
 import com.thadocizn.brav.services.RetroInstance
 import kotlinx.android.synthetic.main.activity_courtcase_form.*
+import kotlinx.android.synthetic.main.activity_courtcase_form.etCaseNotes
+import kotlinx.android.synthetic.main.activity_courtcase_form.etDescription
+import kotlinx.android.synthetic.main.activity_courtcase_form.etDisputeAmount
+import kotlinx.android.synthetic.main.activity_courtcase_form.etPartiesContactInfo
+import kotlinx.android.synthetic.main.activity_courtcase_form.etPartiesInvolved
+import kotlinx.android.synthetic.main.activity_courtcase_form.spDisputeCategory
+import kotlinx.android.synthetic.main.activity_other_case.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -30,6 +38,9 @@ class CourtCaseFormActivity : AppCompatActivity() {
 
                 }
             }
+
+        setupSpinner()
+
         btnCourtCase.setOnClickListener {
 
             val case: Case = case()
@@ -50,7 +61,7 @@ class CourtCaseFormActivity : AppCompatActivity() {
         val courtNumber = etCaseNum.text.toString()
         val description = etDescription.text.toString()
         val disputeAmount = etDisputeAmount.text.toString()
-        val disputeCategory = etDisputeCategory.text.toString()
+        val disputeCategory = spDisputeCategory.selectedItem.toString()
         val partiesContactInfo = etPartiesContactInfo.text.toString()
         val partiesInvolved = etPartiesInvolved.text.toString()
 
@@ -70,6 +81,17 @@ class CourtCaseFormActivity : AppCompatActivity() {
             caseDeclinedAt
         )
         return case
+    }
+
+    private fun setupSpinner() {
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.specialization,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spDisputeCategory.adapter = adapter
+        }
     }
 
     private fun createCase(case: Case) {
--- brav/app/src/main/java/com/thadocizn/brav/views/OtherCaseActivity.kt
+++ brav/app/src/main/java/com/thadocizn/brav/views/OtherCaseActivity.kt
@@ -2,10 +2,12 @@ package com.thadocizn.brav.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.thadocizn.brav.R
 import com.thadocizn.brav.models.Case
 import com.thadocizn.brav.services.RetroInstance
+import kotlinx.android.synthetic.main.activity_mediator.*
 import kotlinx.android.synthetic.main.activity_other_case.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -24,20 +26,26 @@ class OtherCaseActivity : AppCompatActivity() {
                 if (task.isSuccessful) {
                     idToken = task.result!!.token.toString()
                     println(idToken.toString())
-
-
                 } else {
-
                     // Handle error -> task.getException();
-
                 }
             }
+        setupSpinner()
+
         btnSend.setOnClickListener {
-
             val case: Case = case()
-
             createCase(case)
+        }
+    }
 
+    private fun setupSpinner() {
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.specialization,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spDisputeCategory.adapter = adapter
         }
     }
 
@@ -52,7 +60,7 @@ class OtherCaseActivity : AppCompatActivity() {
         val courtNumber = ""
         val description = etDescription.text.toString()
         val disputeAmount = etDisputeAmount.text.toString()
-        val disputeCategory = etDisputeCategory.text.toString()
+        val disputeCategory = spDisputeCategory.selectedItem.toString()
         val partiesContactInfo = etPartiesContactInfo.text.toString()
         val partiesInvolved = etPartiesInvolved.text.toString()
 
