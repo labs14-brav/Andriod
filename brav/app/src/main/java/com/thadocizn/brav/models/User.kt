ba0b143326d86d419dc2d11bf9e01133c71865be
ThaDocIzN <rodan72@msn.com>
2019-08-12 11:09:48 -0500

    updated user model to match web

--- brav/app/src/main/java/com/thadocizn/brav/models/User.kt
+++ brav/app/src/main/java/com/thadocizn/brav/models/User.kt
@@ -3,4 +3,20 @@ package com.thadocizn.brav.models
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class User(val id:Int,val type:String? = null , val email:String, val uid:String)
+data class User(
+    val id:Int,
+    val type:String? = null ,
+    val email:String,
+    val uid:String,
+    val license:String,
+    val price:Int,
+    val experience:String,
+    val specialization:String,
+    val language:String,
+    val professional_bio:String,
+    val name:String,
+    val deactivated_at:String,
+    val mediator_accepted_at:String,
+    val mediator_declined_at: String,
+    val created_at:String,
+    val updated_at:String)
